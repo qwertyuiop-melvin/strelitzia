f1621f121bd1931f386020277b82f99450aa4887
strelitziatrio <strelitziatriomusic@gmail.com>
2025-06-01 19:52:02 -0400

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,29 +1,35 @@
-import { defineStackbitConfig } from '@stackbit/sdk';
+import { defineStackbitConfig } from '@stackbit/types';
+import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
   contentSources: [
-    {
+    new GitContentSource({
       name: 'content',
-      type: 'git',
-      connection: {
-        repositoryId: 'strelitziatrio/strelitziatrio.com'
-      },
+      rootPath: __dirname,
+      contentDirs: ['content'],
       models: [
         {
           name: 'page',
-          type: 'page',
+          type: 'page', // This is crucial for visual editing
           labelField: 'title',
           urlPath: '/{slug}',
-          filePathPattern: 'content/posts/{slug}.md'
+          filePath: 'content/posts/{slug}.md', // Changed from filePathPattern to filePath
+          fields: [
+            { name: 'title', type: 'string', required: true },
+            { name: 'content', type: 'markdown' } // Add your content field
+          ]
         }
-      ]
-    }
+      ],
+    })
   ],
-  siteMap: async ({ contentSource }) => {
-    const pages = await contentSource.getAllEntries({ modelType: 'page' });
-    return pages.map((page) => ({
-      urlPath: `/${page.slug}`,
-      entryId: page.id
-    }));
+  siteMap: ({ documents }) => {
+    return documents
+      .filter(document => document.modelName === 'page')
+      .map(document => ({
+        urlPath: `/${document.slug}`,
+        stableId: document.id,
+        document,
+        isHomePage: document.slug === 'home'
+      }));
   }
 });
